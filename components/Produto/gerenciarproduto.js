0c3bc0f1cac064306696e9e4dd5b902daa326fb3
Ana-Pimenta-Conceicao <ana.pimenta120505@gmail.com>
2023-05-16 17:05:42 -0300

    Gerenciar Produtos

--- components/Produto/gerenciarproduto.js
+++ components/Produto/gerenciarproduto.js
@@ -1,67 +1,111 @@
 import React, { useState, useEffect, useRef } from 'react'; 
-
 import { 
-
     View, Text, StyleSheet, 
-
     TouchableOpacity, Keyboard, FlatList, ActivityIndicator 
-
 } from 'react-native'; 
-
 import { TextInput } from 'react-native-paper'; 
 
-export default function GerenciarProdutos(){
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-    const [nome, setNome] = useState(''); 
-    const [descricao, setDescricao] = useState(''); 
-    const [tipo, setTipo] = useState(''); 
-    const [preco, setPreco] = useState(''); 
-    const [key, setKey] = useState(''); 
+export default function GerenciarProdutos() { 
+    const [nome, setNome] = useState('');  
+    const [marca, setMarca] = useState('');  
+    const [valor, setValor] = useState('');  
+    const [cor, setCor] = useState('');  
+    const [key, setKey] = useState('');  
+
     return ( 
-
         <View style={styles.container}> 
-
             <TextInput 
-                placeholder='Nome' 
-                left={<TextInput.Icon icon="nome" />} 
+                placeholder='Produto' 
+                left={<TextInput.Icon icon="guitar-acoustic" />} 
                 maxLength={40} 
                 style={styles.input} 
-                onChangeText={(texto) => setName(texto)} 
-                value={name} 
+                onChangeText={(text) => setNome(text)} 
+                value={nome} 
             /> 
 
             <TextInput 
-                placeholder='Descricao' 
-                left={<TextInput.Icon icon="descricao" />} 
+                placeholder='Marca' 
+                left={<TextInput.Icon icon="sale" />} 
                 style={styles.input} 
-                onChangeText={(texto) => setBrand(texto)} 
-                value={brand} 
-
+                onChangeText={(text) => setMarca(text)} 
+                value={marca} 
             /> 
-            
-            <TextInput 
-                placeholder='Tipo' 
-                left={<TextInput.Icon icon="tipo" />} 
-                style={styles.input} 
-                onChangeText={(texto) => setColor(texto)} 
-                value={color} 
 
-            />  
             <TextInput 
-                placeholder='Preço' 
+                placeholder='Preço (R$)' 
                 left={<TextInput.Icon icon="sack" />} 
                 style={styles.input} 
-                onChangeText={(texto) => setPrice(texto)} 
-                value={price} 
-
+                onChangeText={(text) => setValor(text)} 
+                value={valor} 
             /> 
 
-                            
+            <TextInput 
+                placeholder='Cor' 
+                left={<TextInput.Icon icon="color" />} 
+                style={styles.input} 
+                onChangeText={(text) => setCor(text)} 
+                value={cor} 
+            />                  
 
         </View> 
-
     ); 
-
 } 
 
- 
+const styles = StyleSheet.create({ 
+
+    container: { 
+        flex: 1, 
+        margin: 10, 
+    }, 
+
+    input: { 
+        borderWidth: 1, 
+        borderColor: '#121212', 
+        height: 40, 
+        fontSize: 13, 
+        borderRadius: 8 
+    }, 
+
+    separator: { 
+        marginVertical: 5, 
+    }, 
+
+    button: { 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        backgroundColor: '#3ea6f2', 
+        borderWidth: 0.5, 
+        borderColor: '#fff', 
+        height: 40, 
+        borderRadius: 5, 
+        margin: 5, 
+    }, 
+
+    buttonImageIconStyle: { 
+        padding: 10, 
+        margin: 5, 
+        height: 25, 
+        width: 25, 
+        resizeMode: 'stretch', 
+    }, 
+
+    buttonTextStyle: { 
+        color: '#fff', 
+        marginBottom: 4, 
+        marginLeft: 100, 
+        fontSize: 20 
+    }, 
+
+    buttonIconSeparatorStyle: { 
+        backgroundColor: '#fff', 
+        width: 1, 
+        height: 20, 
+    }, 
+
+    listar: { 
+        fontSize: 20, 
+        textAlign: 'center' 
+    } 
+}); 
